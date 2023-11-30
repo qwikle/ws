98d269de9559ddc7554d13584f7fa1790382cb55
Qwikle <qwikle.ziko@gmail.com>
2023-11-30 16:08:45 +0100

    refactor: try the connection with the leave

--- main.go
+++ main.go
@@ -3,40 +3,27 @@ package main
 import (
 	"fmt"
 	"net/http"
-
-	"github.com/google/uuid"
 )
 
 func main() {
 	ws := NewSocketServer()
 			ws.OnConnect(func (socket *Socket) {
-			socket.Send("welcome", "Welcome to the server")
-			ws.ExceptTo("new_user", "New user joined", []uuid.UUID{socket.Id})
+			fmt.Printf("%d\n",ws.CountSockets())
 		})
-		ws.OnDisconnect(func (socket *Socket) {
-			ws.Broadcast("user_left", "User left")
-		})
-
-		ws.On("ping", func (data string, socket *Socket) {
-			fmt.Print(data) 
-			socket.Send("pong", "casse toi de la")
+		ws.OnDisconnect(func (reason string) {
+			fmt.Printf("%d\n",ws.CountSockets())
+			fmt.Printf("%s\n",reason)
 		})
 
-		ws.On("byebye", func (data string, socket *Socket) {
-			socket.Send("byebye", "casse toi de la")
+		ws.On("leave", func (data string, socket *Socket) {
+			socket.Send("leave", "See you soon")
 			socket.Close()
-			fmt.Print(ws.CountSockets())
 		})
+
 http.HandleFunc("/ws", ws.HandleWS)
  fmt.Printf("Server started at port 8080\n")
 
-http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/" { 
-	ws.Broadcast("new_user", "New Http user")
-	w.Write([]byte("Hello world"))
-	}
-})
 
  http.ListenAndServe(":8080", nil)
-
+ 
 }
